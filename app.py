1998949157b4a491724333d0cb5a77ad53b9a033
techguru <profile.dghosh@gmail.com>
2023-09-01 23:08:50 -0400

    12th msg - trying fix key hidden issue

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from langchain.llms import HuggingFaceHub
 from dotenv import load_dotenv
 load_dotenv()
 import os
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HF_API_TOKEN")
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
 
 #Function to return the response
